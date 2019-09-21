c8931cea3e8e9f4986e03bd75b153c8a3716cea7
MYiLA <polus180@ya.ru>
2019-09-21 22:10:11 +0300

    Рефактринг

--- js/setup.js
+++ js/setup.js
@@ -12,29 +12,27 @@ document.querySelector('.setup-similar').classList.remove('hidden');
 
 var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var WIZARD_NAMES = [];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var generateRandomId = function (arr) {
-  var index = Math.round(Math.random() * (arr.length - 1));
-  return index;
+  return Math.round(Math.random() * (arr.length - 1));
 };
 
 var wizards = [];
 
-for (var j = 0; j < NAMES.length; j++) {
-  var randomName = generateRandomId(NAMES);
-  var randomSurname = generateRandomId(SURNAMES);
-  WIZARD_NAMES [j] = NAMES[randomName] + ' ' + SURNAMES[randomSurname];
-}
-
-for (var a = 0; a < wizardsNumber; a++) {
-  wizards[a] = {
-    name: WIZARD_NAMES[a],
+var createWizard = function () {
+  var nameId = generateRandomId(NAMES);
+  var surnameId = generateRandomId(SURNAMES);
+  return {
+    name: NAMES[nameId] + ' ' + SURNAMES[surnameId],
     coatColor: COAT_COLORS[generateRandomId(COAT_COLORS)],
     eyesColor: EYES_COLORS[generateRandomId(EYES_COLORS)]
   };
+};
+
+for (var i = 0; i < wizardsNumber; i++) {
+  wizards[i] = createWizard();
 }
 
 var renderWizard = function (wizard) {
@@ -48,8 +46,8 @@ var renderWizard = function (wizard) {
 };
 
 var fragment = document.createDocumentFragment();
-for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
+for (var j = 0; j < wizards.length; j++) {
+  fragment.appendChild(renderWizard(wizards[j]));
 }
 similarListElement.appendChild(fragment);
 
